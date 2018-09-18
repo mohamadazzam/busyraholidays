12ed9ad69faefcfe19c0637f59c6b98f6f2ea5a0
Mohamad Azzam <am4nz@aol.com>
2018-09-18 20:22:12 +0800

    add 404

---
+++ application/controllers/custom404 .php
@@ -0,0 +1,16 @@
+<?php
+class custom404 extends CI_Controller
+{
+	public function __construct()
+	{
+	        parent::__construct();
+	}
+ 
+	public function index()
+	{
+		$this->output->set_status_header('404');
+		$this->data['content'] = 'custom404view';  // View name
+		$this->load->view('template', $data);
+	}
+}
+?>
